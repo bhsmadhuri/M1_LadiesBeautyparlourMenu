e117e801c1ee99b039245f82c4a27d3d5368e8f5
bhsmadhuri <101574451+bhsmadhuri@users.noreply.github.com>
2022-03-31 12:28:19 +0530

    Added Header File

---
+++ 3_Implementation/inc/functionmain.h
@@ -0,0 +1,39 @@
+#ifndef __functionmain_H__
+#define __functionmain_H__
+ 
+/* Main Contents */
+void Threading();
+void Waxing();
+void details();
+void Bleach();
+void ManiCure();
+void Facial();
+void Haircut();
+void Makeup();
+void Mehandi();
+/* Main Contents */
+
+/* Sub Contents */
+void EyeRows(); //Threading
+void FullFace();
+
+void FullArms(); //Waxing
+void FullLegs();
+void FullBody();
+
+void Fullarms(); //Bleach
+void Fulllegs();
+void Fullbody();
+
+void Herbal(); //Alovera
+void Strawberry();
+void Alovera();
+void Fruit();
+
+void BoysCut(); //HairCut
+void HairSpa();
+void HairPack();
+/* Sub Contents */
+
+
+#endif 
---
+++ 3_Implementation/inc/mainsource.h
@@ -0,0 +1,17 @@
+#ifndef __mainsource_H__
+#define __mainsource_H__
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+#include<string.h>
+
+struct PersonalDetails
+{
+    char name[15];
+    char email[15];
+    char address[100];
+    long long int phonenumber;
+};
+//struct PersonalDetails B;
+#endif
